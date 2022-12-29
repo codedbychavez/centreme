5ad8bd0544890027596e998b7c146f6199590be2
codedbychavez <codedbychavez@gmail.com>
2022-12-28 22:22:41 -0300

    Init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar">
+    <ol class="nav-items">
+      <li class="nav-item">
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink to="/about">About</RouterLink>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
